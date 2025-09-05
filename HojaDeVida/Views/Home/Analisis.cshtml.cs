2f3b03d23ddbb06fa8af4c27f2225f310491af7c
Paula85Garcia <michellgarciagarcia85@gmail.com>
2025-09-05 09:06:27 -0500

    Primera version HV C#

---
+++ HojaDeVida/Views/Home/Analisis.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HojaDeVida.Views.Home
+{
+    public class AnalisisModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HojaDeVida/Views/Home/Diseño.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HojaDeVida.Views.Home
+{
+    public class DiseñoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
